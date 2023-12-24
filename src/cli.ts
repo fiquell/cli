90460b26d1a277367b2add3437d2fc5b0c646895
Fiquell <fiquellh@gmail.com>
2023-12-25 05:54:42 +0800

    added random uuid command

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 import { Command } from "commander";
 import { copy } from "./commands/copy";
+import { uuid } from "./commands/uuid";
 import { main } from "./main";
 
 const program = new Command();
@@ -9,5 +10,6 @@ const program = new Command();
 program.action(main);
 
 program.command("copy").description("copy content to clipboard").action(copy);
+program.command("uuid").description("generate random uuid").action(uuid);
 
 program.parse(process.argv);
