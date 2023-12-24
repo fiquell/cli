0ab191104fa6e7bc54d745ce4b2e76c54a1373a1
Fiquell <fiquellh@gmail.com>
2023-12-25 05:53:44 +0800

    added copy command

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { copy } from "./commands/copy";
 import { main } from "./main";
 
 const program = new Command();
 
 program.action(main);
+
+program.command("copy").description("copy content to clipboard").action(copy);
+
 program.parse(process.argv);
---
+++ src/commands/copy.ts
@@ -0,0 +1,5 @@
+import { exec } from "../utils/exec";
+
+export const copy = () => {
+  exec("xclip -selection clipboard");
+};
