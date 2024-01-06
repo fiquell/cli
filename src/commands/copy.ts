49136d1e675ea736dc4a3cf17410b2a6de9a1f2f
Fiquell <fiquellh@gmail.com>
2024-01-07 03:11:26 +0800

    restore previous copy function

--- src/commands/copy.ts
+++ src/commands/copy.ts
@@ -1,9 +1,5 @@
 import { exec } from "../utils/exec";
 
-export const copy = (text?: string) => {
-  if (text) {
-    exec(`echo ${text} | xclip -selection clipboard`);
-  } else {
-    exec("xclip -selection clipboard");
-  }
+export const copy = () => {
+  exec("xclip -selection clipboard");
 };
