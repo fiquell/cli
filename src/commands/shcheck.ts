96c29568211cb854cbcf75540c580ea4377f9368
Fiquell <fiquellh@gmail.com>
2024-03-10 04:34:00 +0800

    allow external sources

--- src/commands/shcheck.ts
+++ src/commands/shcheck.ts
@@ -4,10 +4,8 @@ export const shcheck = async (files: string[]) => {
   const commands = files.map((file) => [
     `shellharden --replace ${file}`,
     `shfmt --write --indent 2 ${file}`,
-    `shellcheck ${file}`,
+    `shellcheck -x ${file}`,
   ]);
 
-  const flattenedCommands = commands.flat();
-
-  await Promise.all(flattenedCommands.map((command) => exec(command)));
+  await Promise.all(commands.flat().map((command) => exec(command)));
 };
