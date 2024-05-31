edacc2e61a62b01b9caf1002044e3d25ef61c928
Fiquell <fiquellh@gmail.com>
2024-05-31 21:18:55 +0800

    change to modules

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
+    printWidth: 120,
     semi: false,
     tabWidth: 4,
-    trailingComma: "none"
 }
 
-module.exports = config
+export default config
